97496e7d28bf5c055d080f1cc2b4ec52da3215b3
Andrei Shneider <megaherz@bk.ru>
2016-11-11 17:02:59 +0300

    naive get-set

--- cache.go
+++ cache.go
@@ -1,3 +1,37 @@
 package gcache
 
+const NoExpiration int =  -1
 
+type Cache struct {
+	keyValue map[string]interface{}
+}
+
+// Set key to hold the string value.
+// If key already holds a value, it is overwritten, regardless of its type.
+func (c* Cache) Set(key string, value interface{}, expire int) {
+	c.keyValue[key] = value
+}
+
+func (c* Cache) Update(key string, value interface{}) {
+
+}
+
+// Get the value of key.
+// If the key does not exist the special value nil is returned
+func (c* Cache) Get(key string) (interface{}, bool) {
+	if value, ok := c.keyValue[key]; ok {
+		return value, true
+	}
+	return nil, false
+}
+
+func (c* Cache) Del (key string) (exists bool) {
+	delete(c.keyValue, key)
+	return true
+}
+
+func NewCache() (*Cache) {
+	return &Cache{
+		keyValue:make(map[string]interface{}),
+	}
+}
--- cache_test.go
+++ cache_test.go
@@ -1,1 +1,29 @@
 package gcache
+
+import "testing"
+
+func TestKeyValue(t *testing.T) {
+
+	value := 24
+	key := "key1"
+
+	cache := NewCache()
+ 	cache.Set(key, value, 5)
+	returnedValue, exist := cache.Get(key)
+
+	if (!exist) {
+		t.Error("Value with key", key, "does not exist" )
+	}
+
+	if (value != returnedValue) {
+		t.Error("Expected value", value, "is not equal to", returnedValue)
+	}
+}
+
+func TestList(t *testing.T) {
+
+}
+
+func TestDict(t *testing.T) {
+
+}

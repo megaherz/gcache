00ca7d3e1a232a219c0566f144809b005f119a6a
Andrei Shneider <megaherz@bk.ru>
2016-11-12 17:13:20 +0300

    Key-value cache without sync and expired items removing

--- cache.go
+++ cache.go
@@ -1,37 +1,114 @@
 package gcache
 
-const NoExpiration int =  -1
+import (
+ 	"time"
+	"errors"
+)
+
+var ErrKeyNotFound = errors.New("Key Not Found")
+
+type item struct {
+	value interface{}
+	ttl time.Duration
+	expireAt time.Time
+}
 
 type Cache struct {
-	keyValue map[string]interface{}
+	items map[string]*item
 }
 
-// Set key to hold the string value.
-// If key already holds a value, it is overwritten, regardless of its type.
-func (c* Cache) Set(key string, value interface{}, expire int) {
-	c.keyValue[key] = value
-}
 
-func (c* Cache) Update(key string, value interface{}) {
+func (c* Cache) getItem (key string) (*item, bool) {
+	if item, ok := c.items[key]; ok {
+		expired := item.expireAt.Before(time.Now())
 
-}
+		if (expired) {
+			return nil, false
+		}
 
-// Get the value of key.
-// If the key does not exist the special value nil is returned
-func (c* Cache) Get(key string) (interface{}, bool) {
-	if value, ok := c.keyValue[key]; ok {
-		return value, true
+		return item, true
 	}
 	return nil, false
 }
 
-func (c* Cache) Del (key string) (exists bool) {
-	delete(c.keyValue, key)
-	return true
+// Get the value of key.
+// If the key does not exist the special value nil is returned
+func (c* Cache) Get(key string) (interface{}, error) {
+	if item, ok := c.getItem(key); ok {
+		return item.value, nil
+	}
+
+	return nil, ErrKeyNotFound
+}
+
+// Get the Tll (Time to live) of the key
+func (c* Cache) Ttl(key string) (time.Duration, error) {
+	item, exists := c.getItem(key)
+	if !exists {
+		return -1, ErrKeyNotFound
+	}
+
+	return item.ttl, nil
+}
+
+// Set key to hold the value.
+// If key already holds a value, it is overwritten, regardless of its type.
+func (c* Cache) Set(key string, value interface{}, ttl time.Duration) {
+
+	expireAt := time.Now().Add(ttl)
+	c.items[key] = &item {
+		value: value,
+		ttl: ttl,
+		expireAt: expireAt,
+	}
+}
+
+// Update the value of the key
+func (c* Cache) Update(key string, value interface{}) error {
+	item, exists := c.getItem(key)
+	if !exists {
+		return ErrKeyNotFound
+	}
+
+	c.Set(key, value, item.ttl)
+	return nil
+}
+
+// Update the value of the key as well as TTL (Time to live)
+func (c* Cache) UpdateWithTll(key string, value interface{}, ttl time.Duration) error{
+	_, exists := c.getItem(key)
+	if !exists {
+		return ErrKeyNotFound
+	}
+
+	c.Set(key, value, ttl)
+	return nil
+}
+
+func (c* Cache) Del (key string) (err error) {
+
+	if _, ok := c.getItem(key); ok {
+		delete(c.items, key)
+	} else {
+		err = ErrKeyNotFound
+	}
+
+	return err
+}
+
+func (c* Cache) Keys() ([]string) {
+
+	keys := make([]string, len(c.items))
+	i := 0
+	for k := range c.items {
+		keys[i] = k
+		i++
+	}
+	return keys
 }
 
 func NewCache() (*Cache) {
 	return &Cache{
-		keyValue:make(map[string]interface{}),
+		items:make(map[string] *item),
 	}
 }
--- cache_test.go
+++ cache_test.go
@@ -1,17 +1,35 @@
 package gcache
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestKeyValue(t *testing.T) {
+func TestCache_Keys(t *testing.T) {
+	cache := NewCache()
+
+	cache.Set("key1", "value", time.Second)
+	cache.Set("key2", "value", time.Second)
+	cache.Set("key3", "value", time.Second)
+
+	keys := cache.Keys()
+
+	if (len(keys) != 3) {
+		t.Error("Expected is", 3, "but actual is", len(keys))
+	}
+}
+
+func TestCache_Get(t *testing.T) {
 
 	value := 24
 	key := "key1"
 
 	cache := NewCache()
- 	cache.Set(key, value, 5)
-	returnedValue, exist := cache.Get(key)
+ 	cache.Set(key, value, 5 * time.Second)
+	returnedValue, err := cache.Get(key)
 
-	if (!exist) {
+
+	if (err == ErrKeyNotFound) {
 		t.Error("Value with key", key, "does not exist" )
 	}
 
@@ -20,10 +38,87 @@ func TestKeyValue(t *testing.T) {
 	}
 }
 
-func TestList(t *testing.T) {
+func TestCache_Del(t *testing.T) {
+	key := "key1"
+
+	cache := NewCache()
+	cache.Set(key, "value", time.Second)
+	err := cache.Del(key)
+
+	if (len(cache.Keys()) != 0 || err != nil) {
+		t.Error("The key has not been removed")
+	}
+
+	err = cache.Del(key)
+
+	if (err != ErrKeyNotFound) {
+		t.Error("The key still exists in Cache")
+	}
 
 }
 
-func TestDict(t *testing.T) {
+func TestCache_Update(t *testing.T) {
+
+	key := "key1"
+	expectedValue := "expected"
+
+	cache := NewCache()
+	cache.Set(key, "value", 10 * time.Microsecond)
+
+	err := cache.Update(key, expectedValue)
+
+	if (err != nil) {
+		t.Error("Failed to update")
+	}
+
+	returnedValue, _ := cache.Get(key)
+
+	if returnedValue != expectedValue {
+		t.Error("Failed to update the value. The returned value is", returnedValue)
+	}
+}
+
+func TestCache_UpdateWithTll(t *testing.T) {
+	key := "key1"
+	expectedTtl := time.Second * 5
+	expectedValue := "expected"
+
+	cache := NewCache()
+	cache.Set(key, "value", 10 * time.Microsecond)
+
+	err := cache.UpdateWithTll(key, expectedValue, expectedTtl)
+
+	if (err != nil) {
+		t.Error("Failed to update")
+	}
+
+	returnedValue, _ := cache.Get(key)
+
+	if returnedValue != expectedValue {
+		t.Error("Failed to update the value. The returned value is", returnedValue)
+	}
+
+	returnedTtl, _ := cache.Ttl(key)
+
+	if returnedTtl != expectedTtl {
+		t.Error("Failed to update the tll. The returned tll is", returnedTtl)
+	}
+
+}
+
+func TestCache_Expiration(t *testing.T) {
+
+	key := "key1"
+
+	cache := NewCache()
+	cache.Set(key, "value", 10 * time.Microsecond)
+
+	time.Sleep(time.Second)
+
+	_, err :=  cache.Get(key)
+
+	if (err != ErrKeyNotFound) {
+		t.Error("The key has not been expired")
+	}
 
 }

28431a6c100b2ca03c51fb8e7ba4f2f62abc3d4a
Andrei Shneider <megaherz@bk.ru>
2016-11-15 15:28:23 +0300

    TestHashesHandler_HSetGSet

--- server/handlers/hashesHandler_test.go
+++ server/handlers/hashesHandler_test.go
@@ -1,1 +1,59 @@
 package handlers
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"gcache"
+	"io/ioutil"
+)
+
+func TestHashesHandler_HSetGSet(t *testing.T) {
+
+	const haskKey  = "hashKey"
+	const key  = "key"
+	const value  = "value"
+
+	handler := HashesHandler{}
+	handler.Init(gcache.NewCache())
+
+	ts := httptest.NewServer(http.HandlerFunc(handler.Handle))
+	defer ts.Close()
+
+	url := ts.URL + "?hashKey=" + haskKey + "&key=" + key + "&value=" + value
+
+	rr, err := http.Post(url, "", nil)
+
+	if (err != nil){
+		t.Fatalf("http.Get(%q) unexpected error: %v", url, err)
+	}
+
+	// Check the status code is what we expect.
+	if status := rr.StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	url = ts.URL + "?hashKey=" + haskKey + "&key=" + key
+
+	rr, err = http.Get(url)
+
+	if (err != nil){
+		t.Fatalf("http.Get(%q) unexpected error: %v", url, err)
+	}
+
+	// Check the status code is what we expect.
+	if status := rr.StatusCode; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	actual, err := ioutil.ReadAll(rr.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value != string(actual) {
+		t.Errorf("Expected the message '%s'\n", value)
+	}
+
+}
